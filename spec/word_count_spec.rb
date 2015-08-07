d8caa3d6407b413be98a60ef34f84e86797cc7f6
Brigham <brigham.thompson@gmail.com>
2015-08-07 11:04:17 -0700

    add 2 new spec test: one for lowercase & one for counting instances

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,11 +2,19 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
+  it('changes entered text to all lowercase') do
+    expect(('Hello world').word_count()).to(eq('hello world'))
+  end
+
   it('breaks a string into an array') do
     expect(('hello world').word_count()).to(eq(['hello','world']))
   end
 
   it('checks to see if a given word is in the array') do
     expect('hello world'.word_count('hello')).to(eq('hello'))
+  end
+
+  it('counts every instance of a word') do
+    expect('monday monday so good to me'.word_count('monday')).to(eq(2))
   end
 end
